d1880b1f046233d8ff5de8069168e75ed24a4802
c7877 <111748626+c7877@users.noreply.github.com>
2022-09-02 21:07:04 +0800

    Update py_bilibili.py

--- T4/plugin/py_bilibili.py
+++ T4/plugin/py_bilibili.py
@@ -20,7 +20,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"Zard": "Zard",
+			"电影解说": "电影解说超清",
 			"玩具汽车": "玩具汽车",
 			"儿童": "儿童",
 			"幼儿": "幼儿",
